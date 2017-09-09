d9b7cb24228872e4d3d97174d9a9bf7bc2be94cd
Rob Thomas <robtom_5@mac.com>
2017-09-09 16:24:29 +0100

    [Add] Added entries and improved layout
    Added classification of common, uncommon and rare rooms and generator
    adjusted to generate according to this, also allow specification of how many
    of each category to build.
    
    Reduced number of props spawned for rooms with low amount of options due
    to excessive duplicates being created. Reccomend fleshing out further
    before increasing the prop count again.

--- building.rb
+++ building.rb
@@ -8,16 +8,25 @@ class Building
     attr_reader :type
     def initialize
         yaml = YAML.load(File.read('tables/building_components/building_types.yaml'))
-        @type = yaml['building_type'].sample.to_str
+        @type = yaml['building_type'].sample
+        puts @type
+        @no_com = yaml[@type]['num_common'].sample
+        @no_unc = yaml[@type]['num_uncommon'].sample
+        @no_rar = yaml[@type]['num_rare'].sample
 
     end
 
     def to_s
         yaml = YAML.load(File.read('tables/building_components/building_types.yaml'))
-        Building_room.new(yaml[@type+'_rooms'].sample.to_str)
+        
+        @no_com.times {Building_room.new(yaml[@type]['common_rooms'].sample.to_str)}
+        @no_unc.times {Building_room.new(yaml[@type]['uncommon_rooms'].sample.to_str)}
+        @no_rar.times {Building_room.new(yaml[@type]['rare_rooms'].sample.to_str)}
 
         string = ""
-        # [4,5].sample.times {string = string + yaml[@type+'_rooms'].sample.to_str + "\n"}
+        # @no_com.times {string = string + yaml[@type]['uncommon_rooms'].sample.to_str + "\n"}
+        # @no_unc.times {string = string + yaml[@type]['uncommon_rooms'].sample.to_str + "\n"}
+        # @no_rar.times {string = string + yaml[@type]['uncommon_rooms'].sample.to_str + "\n"}
         return string
     end
 end
@@ -27,10 +36,10 @@ class Building_room
         rooms = YAML.load(File.read('tables/building_components/room_types.yaml'))
         @room_details = rooms[type]
         # puts type
-        puts @room_details['name']
-        puts @room_details['size'].sample
-        puts @room_details['description']
-        @room_details['prop_number'].times {puts @room_details['props'].sample}
+        puts @room_details['size'].sample + ' ' + @room_details['name']
+        # puts @room_details['description']
+        @room_details['prop_number'].times {puts '   ' + @room_details['props'].sample}
+        puts '-------------------------------'
     end
 end
 
